d7641ae39bd6e0817fdb854fe1c30230a08d75ad
Peter <youngeunpc@yahoo.com>
2024-12-27 08:38:20 -0300

    Ball Animation

--- track.js
+++ track.js
@@ -7,7 +7,7 @@ class Track {
     getPosition(offset) {
         return {
             x: this.center.x + Math.cos(offset) * this.radius,
-            y: this.center.y + Math.sin(offset) * this.radius
+            y: this.center.y - Math.sin(offset) * this.radius
         }
     }
 
